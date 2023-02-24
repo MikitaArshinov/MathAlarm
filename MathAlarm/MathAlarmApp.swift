124b062da2467e6c67a5b81d7cff96f7a6a3c7df
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2023-02-25 00:30:20 +0300

    Initial commit

---
+++ MathAlarm/MathAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathAlarmApp.swift
+//  MathAlarm
+//
+//  Created by Nikita Arshinov on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MathAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
